61f3070d1cd084129c83c3cc8036eac37e5432d5
teranixbq <hanieffathulb03@gmail.com>
2024-02-15 16:46:40 +0700

    fix : run time complexty to 6ms

--- Easy/01. Two Sum/twosum.go
+++ Easy/01. Two Sum/twosum.go
@@ -1,29 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	nums := []int{1, 2}
-	target := 3
+	nums := []int{4, 3, 5, 6}
+	target := 10
 
 	result := TwoSum(nums, target)
 	fmt.Printf("%d + %d = %d\n", nums[result[0]], nums[result[1]], target)
 	fmt.Println("---------------")
 	fmt.Printf("%d index ke %d\n",nums[result[0]],result[0])
 	fmt.Printf("%d index ke %d\n",nums[result[1]],result[1])
+
 }
 
 func TwoSum(nums []int, target int) []int {
+	MapOfKey:= make(map[int]int)
 
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); i++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	for i, num := range nums {
+		complement := target - num
+		
+		if index, ok := MapOfKey[complement]; ok {
+			return []int{index, i}
 		}
+		MapOfKey[num] = i
 	}
-
 	return nil
 }
